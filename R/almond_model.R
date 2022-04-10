5751dcecc74a76369ccef967de78333ad69e79c9
quin-smith <quinlan_smith@yahoo.com>
2022-04-10 14:21:46 -0700

    got the right yield anomalies

--- R/almond_model.R
+++ R/almond_model.R
@@ -17,10 +17,10 @@ almond_model <- function(Tn2, Tn2_coeff1 = -0.015, Tn2_coeff2 = -0.0046,
                          P1, P1_coeff1 = -0.07, P1_coeff2 = 0.0043, 
                          constant = 0.28)
   {
-  yield_anomaly <- (Tn2 * Tn2_coeff1) +
-    (Tn2^2 * Tn2_coeff2) +
-    (P1 * P1_coeff1) +
-    (P1^2 * P1_coeff2) +
+  yield_anomaly <- (Tn2_coeff1 * Tn2) +
+    (Tn2_coeff2 * Tn2**2) +
+    (P1_coeff1 * P1) +
+    (P1_coeff2 * P1**2) +
     constant
   
   return(yield_anomaly)
