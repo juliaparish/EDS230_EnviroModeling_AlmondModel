6b4213dcc95340355e3757faa96f92258dc5d46a
Paloma Cartwright <palomacartwright@gmail.com>
2022-04-11 16:14:49 -0700

    updated the roxygen skeleton for the function

--- R/almond_model.R
+++ R/almond_model.R
@@ -1,17 +1,22 @@
-#' Almond Yield Anomaly Model 
+#' Title Almond Yield Anomaly Model 
+#' 
+#' This function calculates the yearly almond yield anomaly in tons per acre based on the model in the Lobell 2006 paper studied in class.
+#' 
+#' @authors Paloma Cartwright, Julia Parish, Quinn Smith 
 #'
-#' @param mon_temp_min 
-#' @param mon_precip 
-#' @param temp_coeff1 
-#' @param temp_coeff2 
-#' @param precip_coeff1 
-#' @param precip_coeff2 
-#' @param constant 
+#' @param Tn2 minimum temperature (degrees Celsius) for February 
+#' @param Tn2_coeff1 coefficient 1 to be used with Tn2, default set at -0.015
+#' @param Tn2_coeff2 coefficient 2 to be used with Tn2^2, default is set at -0.0046
+#' @param P1 precipitation (mm) for January 
+#' @param P1_coeff1 coefficient 1 to be used with P1, default set at -0.07
+#' @param P1_coeff2 coefficient 2 to be used with P1^2, default set at 0.0043
+#' @param constant default set at 0.28
 #'
-#' @return
-#' @export
+#' @return almond yield anomaly (ton/acre)
 #'
-#' @examples
+#' @examples almond_model(Tn2 = 20, P1 = 8)
+#' @examples almond_model(Tn2 = 20, Tn2_coeff1 = -0.015, Tn2_coeff2 = -0.0046, P1 = 8, P1_coeff1 = -0.07, P1_coeff2 = 0.0043, constant = 0.28)
+#' 
 
 almond_model <- function(Tn2, Tn2_coeff1 = -0.015, Tn2_coeff2 = -0.0046,
                          P1, P1_coeff1 = -0.07, P1_coeff2 = 0.0043, 
